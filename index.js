4bd88b927f9860da761efa95231d4ebdcb6fcced
spencer kelly <spencermountain@gmail.com>
2018-11-20 17:00:38 -0500

    budo-start

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('hello?')
+//comment!
+let el = document.querySelector('body')
+el.innerHTML = '<h2>one.</h2>'
--- src/shapes/line.js
+++
@@ -1,9 +0,0 @@
-const Shape = require('./shape')
-// const d3Shape = require('d3-shape')
-
-class Line extends Shape {
-  constructor(data, obj, world) {
-    super(data, obj, world);
-  }
-}
-module.exports = Line
