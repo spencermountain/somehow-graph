cb11343f94d3929f2cab60c73867eb2e6d32d240
spencer kelly <spencermountain@gmail.com>
2019-05-04 19:40:19 -0400

    use latest somehow-ticks

--- scratch.js
+++ scratch.js
@@ -1,21 +1,16 @@
 const somehow = require('./src')
 
 let w = somehow({
-  el: '#stage',
   height: 300,
   aspect: 'widescreen'
 })
 
 w.line()
-  .set([['Jan 21st 2019', '0%'], ['Jan 22nd 2019', '80']])
+  .set([['Jan 21st 2019', '0%'], ['Jan 22nd 2019', '80%']])
   .color('blue')
 
-w.line()
-  .set([['Jan 10th 2019', '10%'], ['Jan 10th 2019', '80%']])
-  .color('lightgrey')
-
 w.fit()
-w.y.fit(-9, 80)
+w.y.fit(-9, 8000)
 w.x.fit('Jan 1 2019', 'Jan 28 2019')
 w.clip()
 w.x.reverse()
--- src/axis/_ticks.js
+++ src/axis/_ticks.js
@@ -1,27 +1,11 @@
 const spacetimeTicks = require('spacetime-ticks')
-const prettyNum = require('./_prettyNum')
+const somehowTicks = require('somehow-ticks')
 
 const generic = function(axis, n = 5) {
-  n = n === 0 ? 0 : n - 1
   let scale = axis.scale
-  let total = (scale.max || 0) - (scale.min || 0)
-  if (total === 0) {
-    total = 10
-    scale.max = 10
-    scale.min = 0
-  }
-  let ticks = []
-  for (let i = 0; i <= n; i += 1) {
-    let dec = i / n
-    let num = dec * total + (scale.min || 0)
-    let tmp = num - (scale.min || 0)
-    let percent = tmp / total
-    ticks.push({
-      value: percent,
-      pos: scale.scale(num),
-      label: prettyNum(num)
-    })
-  }
+  let start = scale.min || 0
+  let end = scale.max || 0
+  let ticks = somehowTicks(start, end, n)
   return ticks
 }
 
